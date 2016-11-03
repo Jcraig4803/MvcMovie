311fb1d7986aed62b339a0dd50f4a8ed882fae88
Johnathan Craig (jcraig4803) <JCRAIG4803@suscorp.edu>
2016-11-02 20:54:35 -0400

    MvcMovie
    Johnathan Craig MvcMovie

---
+++ MvcMovie0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcMovie0.MvcApplication" Language="C#" %>
---
+++ MvcMovie0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovie0.Startup))]
+namespace MvcMovie0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
